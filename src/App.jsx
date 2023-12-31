5386463a345dd715d3afd2f9c6503f43c0d8f242
MohamedMamdouh-98 <miidom96@gmail.com>
2023-12-31 15:39:30 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,6 @@ import AddProduct from "./pages/AddProduct";
 import EditProduct from "./pages/EditProduct";
 
 const App = () => {
-  // useEffect(() => {
-  //   axios.get("http://localhost:4000/products").then((res) => {
-  //     res.json();
-  //   });
-  // }, []);
   return (
     <>
       <AppBar />
