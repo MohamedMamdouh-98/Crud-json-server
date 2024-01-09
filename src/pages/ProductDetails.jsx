edc773bef4548b37478af72b3b4f6f6c807ed268
MohamedMamdouh-98 <miidom96@gmail.com>
2024-01-09 16:19:27 +0200

    update

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -3,15 +3,20 @@ import { useParams } from "react-router-dom";
 
 const ProductDetails = () => {
   const [product, setProduct] = useState();
-  const {productId} = useParams();
-  useEffect(()=>{
-      fetch(`http://localhost:5000/products/${productId}`)
-        .then((res) => res.json())
-        .then((data) => {
-          setProduct(data);
-        });
-  },[])
-  return <div>{product?.title}</div>;
+  const { productId } = useParams();
+  useEffect(() => {
+    fetch(`http://localhost:5000/products/${productId}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setProduct(data);
+      });
+  }, []);
+  return (
+    <>
+      <h2>{product?.title}</h2>
+      <h5>Price : {product?.price} $</h5>
+    </>
+  );
 };
 
 export default ProductDetails;
